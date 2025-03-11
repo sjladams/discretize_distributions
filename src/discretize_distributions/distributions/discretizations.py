caecec08fd906421627643e7f924d59279e976e5
sjladams <steven.adams.95@hotmail.com>
2025-03-11 22:00:20 +0100

    discretizations.py: introduce Discretizations, formatting, nr_signature_locations -> num_locs

--- src/discretize_distributions/distributions/discretizations.py
+++ src/discretize_distributions/distributions/discretizations.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.hipify.hipify_python import InputError
+from typing import Optional
 
 from discretize_distributions.distributions.multivariate_normal import MultivariateNormal
 from discretize_distributions.distributions.categorical_float import CategoricalFloat
@@ -12,62 +12,63 @@ __all__ = ['DiscretizedMultivariateNormal',
            ]
 
 
-class DiscretizedMultivariateNormal(CategoricalFloat):
-    def __init__(self, norm: MultivariateNormal, **kwargs):
-        if not isinstance(norm, MultivariateNormal):
-            raise ValueError('distribution not of type MultivariateNormal')
-
-        self.dist = norm
-        locs, probs, self.w2 = discretize_multi_norm_dist(norm=norm, **kwargs)
-
-        self.nr_signature_points_realized = probs.shape[-1]   # \todo rename num_locs
-
+class Discretization(CategoricalFloat):
+    def __init__(self,
+                 dist: torch.distributions.Distribution,
+                 probs: torch.Tensor,
+                 locs: torch.Tensor,
+                 w2: Optional[torch.Tensor]):
+        self.dist = dist
+        self.num_locs = probs.shape[-1]
+        self.w2 = w2
         super().__init__(probs, locs)
 
 
-class DiscretizedMixtureMultivariateNormal(CategoricalFloat):
+class DiscretizedMultivariateNormal(Discretization):
+    def __init__(self, norm: MultivariateNormal, num_locs: int):
+        assert isinstance(norm, MultivariateNormal), 'distribution not of type MultivariateNormal'
 
-    def __init__(self, gmm: MixtureMultivariateNormal, **kwargs):
-        if not isinstance(gmm, MixtureMultivariateNormal):
-            raise ValueError('distribution not of type MixtureMultivariateNormal')
-        discretized_component_distribution = discretization_generator(dist=gmm.component_distribution,
-                                                                      **kwargs)
+        locs, probs, w2 = discretize_multi_norm_dist(norm, num_locs)
 
-        probs = torch.einsum('...ms,...m->...ms', discretized_component_distribution.probs,
+        super().__init__(norm, probs, locs, w2)
+
+
+class DiscretizedMixtureMultivariateNormal(Discretization):
+    def __init__(self, gmm: MixtureMultivariateNormal, num_locs: int, **kwargs):
+        assert isinstance(gmm, MixtureMultivariateNormal), 'distribution not of type MixtureMultivariateNormal'
+
+        disc_component_distribution = discretization_generator(gmm.component_distribution, num_locs)
+
+        probs = torch.einsum('...ms,...m->...ms',
+                             disc_component_distribution.probs,
                              gmm.mixture_distribution.probs)
         probs = probs.flatten(start_dim=-2)
-        locs = discretized_component_distribution.locs
+        locs = disc_component_distribution.locs
         locs = locs.reshape(locs.shape[:-3] + (locs.shape[-3:-1].numel(), locs.shape[-1]))
-        if discretized_component_distribution.w2 is not None:
-            self.w2 = torch.einsum('...m,...m->...', gmm.mixture_distribution.probs,
-                                  discretized_component_distribution.w2.pow(2)).sqrt()
-        else:
-            self.w2 = None
-        self.nr_signature_points_realized = discretized_component_distribution.probs.shape[-1]
 
-        super().__init__(probs, locs)
+        if disc_component_distribution.w2 is not None:
+            w2 = torch.einsum('...m,...m->...',
+                              gmm.mixture_distribution.probs,
+                              disc_component_distribution.w2.pow(2)).sqrt()
+        else:
+            w2 = None
+
+        super().__init__(gmm, probs, locs, w2)
 
 
 class DiscretizationGenerator:
-    def __call__(self, dist, num_locs: int = None, nr_signature_points: int = None,
-                            compute_w2: bool=True, **kwargs):
+    def __call__(self, dist, *args, **kwargs):
         """
 
         :param dist:
         :param num_locs:
-        :param nr_signature_points: To be replaced by num_locs. Preserved to guarantee compatibility with old code.
-        :param compute_w2: Redundancy of old approach
         :return:
         """
-        if nr_signature_points is None and num_locs is None:
-            raise InputError('Specify num_locs')
-        elif num_locs is None:
-            num_locs = nr_signature_points
 
         if type(dist) is MultivariateNormal:
-            return DiscretizedMultivariateNormal(dist, num_locs=num_locs, **kwargs)
+            return DiscretizedMultivariateNormal(dist, *args, **kwargs)
         elif type(dist) is MixtureMultivariateNormal:
-            return DiscretizedMixtureMultivariateNormal(dist, num_locs=num_locs, **kwargs)
+            return DiscretizedMixtureMultivariateNormal(dist, *args, **kwargs)
         elif isinstance(dist, CategoricalFloat):
             return dist
         else:
