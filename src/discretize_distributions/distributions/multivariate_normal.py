663b96b481e0b83ada93c3d98b35fe308cdb451d
sjladams <steven.adams.95@hotmail.com>
2025-03-12 17:13:25 +0100

    MultivariateNormal as degenerate generalization of torch equivalent

--- src/discretize_distributions/distributions/multivariate_normal.py
+++ src/discretize_distributions/distributions/multivariate_normal.py
@@ -1,61 +1,38 @@
-from discretize_distributions.tensors import diag_matrix_mult_full_matrix, full_matrix_mult_diag_matrix, eigh, make_sym
-
+import discretize_distributions.tensors as tensors
+from torch.distributions.utils import _standard_normal
+from torch.distributions.multivariate_normal import _batch_mv, _batch_mahalanobis
 import torch
 import math
 
-__all__ = ['MultivariateNormal',]
+__all__ = ['MultivariateNormal']
 
 PRECISION = torch.finfo(torch.float32).eps
-CONST_SQRT_2 = math.sqrt(2)
-CONST_INV_SQRT_2PI = 1 / math.sqrt(2 * math.pi)
-CONST_INV_SQRT_2 = 1 / math.sqrt(2)
-CONST_LOG_INV_SQRT_2PI = math.log(CONST_INV_SQRT_2PI)
-CONST_LOG_SQRT_2PI_E = 0.5 * math.log(2 * math.pi * math.e)
-
 
 class MultivariateNormal(torch.distributions.Distribution):
-    def __init__(self, loc: torch.Tensor, covariance_matrix: torch.Tensor,
-                 inherit_torch_distribution: bool = True, *args, **kwargs):
-        assert not loc.dim() < 1, "loc must be at least one-dimensional."
-        assert not covariance_matrix.dim() < 2, ("covariance_matrix must be at least two-dimensional, "
-                                                 "with optional leading batch dimensions")
+    """
+    Similar to torch.distributions.MultivariateNormal, but allows for degenerative covariance matrices.
+    """
+    def __init__(self, loc: torch.Tensor, covariance_matrix: torch.Tensor):
+        if loc.dim() < 1:
+            raise ValueError("loc must be at least one-dimensional.")
+        if covariance_matrix.dim() < 2:
+            raise ValueError("covariance_matrix must be at least two-dimensional, with optional leading batch dimensions")
 
         batch_shape = torch.broadcast_shapes(covariance_matrix.shape[:-2], loc.shape[:-1])
         event_shape = torch.broadcast_shapes(loc.shape[-1:], covariance_matrix.shape[-1:])
-        covariance_matrix = covariance_matrix.expand(batch_shape + event_shape + event_shape)
-        self.covariance_matrix = covariance_matrix
+        self.covariance_matrix = covariance_matrix.expand(batch_shape + event_shape + event_shape)
         self.loc = loc.expand(batch_shape + event_shape)
 
-        self._sqrt_diag_covariance_matrix = torch.diagonal(covariance_matrix, dim1=-2, dim2=-1).pow(0.5)
+        self.is_covariance_matrix_diagonal = tensors.is_mat_diag(self.covariance_matrix)
 
-        if (covariance_matrix - torch.eye(covariance_matrix.shape[-1]).expand(covariance_matrix.shape)).any():
-            self.diagonal_covariance_matrix = False
-        else:
-            self.diagonal_covariance_matrix = True
+        # Account for possibly degenerative (spd) covariance matrices
+        # (alternatively, one could use the Cholesky decomposition to construct the mahalanobis transformation matrix)
+        eig_vals, eig_vectors = tensors.eigh(self.covariance_matrix)
+        eig_vals_sqrt = (eig_vals.abs() + PRECISION).sqrt() # ensure numerical stability
+        self._mahalanobis_mat = tensors.diag_mat_mult_full_mat(eig_vals_sqrt.reciprocal(), eig_vectors.swapaxes(-1, -2))
+        self._inv_mahalanobis_mat = tensors.full_mat_mult_diag_mat(eig_vectors, eig_vals_sqrt)
 
-        if inherit_torch_distribution:
-            eigvals, eigvectors = eigh(covariance_matrix)
-            self._degenerative_trans_mat = diag_matrix_mult_full_matrix((eigvals.abs() + PRECISION).sqrt().reciprocal(),
-                                                                        eigvectors.swapaxes(-1, -2))
-            self._inv_degenerative_trans_mat = full_matrix_mult_diag_matrix(eigvectors,
-                                                                            (eigvals.abs() + PRECISION).sqrt())
-            degenerative_covariance_matrix = torch.einsum('...ij,...jk,...lk->...il',
-                                                          self._degenerative_trans_mat,
-                                                          self.covariance_matrix,
-                                                          self._degenerative_trans_mat)
-            degenerative_covariance_matrix = make_sym(degenerative_covariance_matrix)
-            degenerative_loc = torch.einsum('...ij,...j->...i', self._degenerative_trans_mat, self.loc)
-            self.degen_mult_normal_torch = torch.distributions.MultivariateNormal(*args,
-                                    covariance_matrix=degenerative_covariance_matrix, loc=degenerative_loc, **kwargs)
-
-            self._batch_shape = self.degen_mult_normal_torch._batch_shape
-            self._event_shape = self.loc.shape[-1:]
-        else:
-            self._batch_shape = batch_shape
-            self._event_shape = event_shape
-
-        super(MultivariateNormal, self).__init__(batch_shape=batch_shape, event_shape=event_shape,
-                                                 validate_args=False)
+        super(MultivariateNormal, self).__init__(batch_shape, event_shape)
 
     @property
     def mean(self):
@@ -63,16 +40,17 @@ class MultivariateNormal(torch.distributions.Distribution):
 
     @property
     def variance(self):
-        return torch.einsum('...ij,...j->...i', self._inv_degenerative_trans_mat.pow(2),
-                            self.degen_mult_normal_torch.variance)
+        return self.covariance_matrix.diagonal(dim1=-2, dim2=-1)
 
     def rsample(self, sample_shape=torch.Size()):
-        return torch.einsum('...ij,...j->...i', self._inv_degenerative_trans_mat,
-                            self.degen_mult_normal_torch.rsample(sample_shape=sample_shape))
+        shape = self._extended_shape(sample_shape)
+        eps = _standard_normal(shape, dtype=self.loc.dtype, device=self.loc.device)
+        return self.loc + _batch_mv(self._inv_mahalanobis_mat, eps)
 
     def log_prob(self, value):
-        return self.degen_mult_normal_torch.log_prob(value=torch.einsum('...ij,...j->...i',
-                                                                        self._degenerative_trans_mat, value))
-
-    def entropy(self):
-        return self.degen_mult_normal_torch.entropy()
+        diff = value - self.loc
+        M = _batch_mahalanobis(self._inv_mahalanobis_mat, diff)
+        half_log_det = (
+            self._inv_mahalanobis_mat.diagonal(dim1=-2, dim2=-1).log().sum(-1)
+        )
+        return -0.5 * (self._event_shape[0] * math.log(2 * math.pi) + M) - half_log_det
