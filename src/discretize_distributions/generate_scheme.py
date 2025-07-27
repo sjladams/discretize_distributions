afcada09b5be650e6830e8eb33566d7d92e40d75
sjladams <steven.adams.95@hotmail.com>
2025-07-27 16:08:09 +0200

    style: rearange functions in generate_schemes.py

--- src/discretize_distributions/generate_scheme.py
+++ src/discretize_distributions/generate_scheme.py
@@ -81,61 +81,6 @@ def generate_grid_scheme_for_multivariate_normal(
 
     return dd_schemes.GridScheme(grid_of_locs, grid_partition)
 
-def axes_from_norm(norm: dd_dists.MultivariateNormal) -> dd_schemes.Axes:
-    """
-    Converts a MultivariateNormal distribution to a discretization Axes object.
-    The Axes object contains the grid of locations, rotation matrix, scales, and offset.
-    """
-    return dd_schemes.Axes(
-        rot_mat=norm.eigvecs,
-        scales=norm.eigvals_sqrt,
-        offset=norm.loc
-    )
-
-def get_optimal_grid_config(
-        eigvals: torch.Tensor,
-        num_locs: int
-    ) -> torch.Tensor:
-    """
-    GRID_CONFIGS provides all non-dominated configs for a number of signature points. The order of the configs match
-    an decrease of eigenvalue over the dimensions, i.e., config (d0, d1, .., dn) assumes eig(do)>=eig(d1)>=eig(dn).
-    The total number of dimensions included per configuration, equals the maximum number dimensions that can create a
-    grid of size signature_points, i.e., equals log2(nr_signature_points).
-    :param eigvals:
-    :param num_locs: number of discretization points, i.e., size of grid.  per discretized Gaussian.
-    :return:
-    """
-    batch_shape = eigvals.shape[:-1]
-    neigh = eigvals.shape[-1]
-    eigvals_sorted, sort_idxs = eigvals.sort(descending=True)    
-
-    if num_locs not in GRID_CONFIGS:
-        if eigvals_sorted.unique().numel() == 1:
-            opt_config = (torch.ones(batch_shape + (neigh,)) * int(num_locs ** (1 / neigh))).to(torch.int64)
-            return opt_config
-
-        num_locs_options = torch.tensor(list(GRID_CONFIGS.keys()), dtype=torch.int)
-        idx_closest_option = torch.where(num_locs_options <= num_locs)[0][-1]
-        num_locs = int(num_locs_options[idx_closest_option])
-        print(f'Grid optimized for size: {num_locs}, requested grid size not available in lookuptables')
-
-    if num_locs == 1:
-        opt_config = torch.empty(batch_shape + (0,)).to(torch.int64)
-    else:
-        costs = GRID_CONFIGS[num_locs]['w2'][..., :neigh] # only select the grids that are relevant for the number of dimensions
-        dims_configs = costs.shape[-1]
-
-        objective = torch.einsum('ij,...j->...i', costs, eigvals_sorted[..., :dims_configs])
-        opt_config_idxs = objective.argmin(dim=-1)
-
-        opt_config = [GRID_CONFIGS[num_locs]['configs'][int(idx)] for idx in opt_config_idxs.flatten()]
-        opt_config = torch.stack(opt_config).reshape(batch_shape + (-1,))
-        opt_config = opt_config[..., :neigh]
-
-    # append grid of size 1 to dimensions that are not yet included in the optimal grid.
-    opt_config = torch.cat((opt_config, torch.ones(batch_shape + (neigh - opt_config.shape[-1],)).to(opt_config.dtype)), dim=-1)
-    return opt_config[sort_idxs]
-
 
 def generate_layered_grid_scheme_for_mixture_multivariate_normal_per_component(
     gmm: dd_dists.MixtureMultivariateNormal,
@@ -270,6 +215,61 @@ def generate_multi_grid_scheme_for_mixture_multivariate_normal(
 
 
 ## --- Utils -----------------------------------------------------------------------------------------------------------
+def axes_from_norm(norm: dd_dists.MultivariateNormal) -> dd_schemes.Axes:
+    """
+    Converts a MultivariateNormal distribution to a discretization Axes object.
+    The Axes object contains the grid of locations, rotation matrix, scales, and offset.
+    """
+    return dd_schemes.Axes(
+        rot_mat=norm.eigvecs,
+        scales=norm.eigvals_sqrt,
+        offset=norm.loc
+    )
+
+def get_optimal_grid_config(
+        eigvals: torch.Tensor,
+        num_locs: int
+    ) -> torch.Tensor:
+    """
+    GRID_CONFIGS provides all non-dominated configs for a number of signature points. The order of the configs match
+    an decrease of eigenvalue over the dimensions, i.e., config (d0, d1, .., dn) assumes eig(do)>=eig(d1)>=eig(dn).
+    The total number of dimensions included per configuration, equals the maximum number dimensions that can create a
+    grid of size signature_points, i.e., equals log2(nr_signature_points).
+    :param eigvals:
+    :param num_locs: number of discretization points, i.e., size of grid.  per discretized Gaussian.
+    :return:
+    """
+    batch_shape = eigvals.shape[:-1]
+    neigh = eigvals.shape[-1]
+    eigvals_sorted, sort_idxs = eigvals.sort(descending=True)    
+
+    if num_locs not in GRID_CONFIGS:
+        if eigvals_sorted.unique().numel() == 1:
+            opt_config = (torch.ones(batch_shape + (neigh,)) * int(num_locs ** (1 / neigh))).to(torch.int64)
+            return opt_config
+
+        num_locs_options = torch.tensor(list(GRID_CONFIGS.keys()), dtype=torch.int)
+        idx_closest_option = torch.where(num_locs_options <= num_locs)[0][-1]
+        num_locs = int(num_locs_options[idx_closest_option])
+        print(f'Grid optimized for size: {num_locs}, requested grid size not available in lookuptables')
+
+    if num_locs == 1:
+        opt_config = torch.empty(batch_shape + (0,)).to(torch.int64)
+    else:
+        costs = GRID_CONFIGS[num_locs]['w2'][..., :neigh] # only select the grids that are relevant for the number of dimensions
+        dims_configs = costs.shape[-1]
+
+        objective = torch.einsum('ij,...j->...i', costs, eigvals_sorted[..., :dims_configs])
+        opt_config_idxs = objective.argmin(dim=-1)
+
+        opt_config = [GRID_CONFIGS[num_locs]['configs'][int(idx)] for idx in opt_config_idxs.flatten()]
+        opt_config = torch.stack(opt_config).reshape(batch_shape + (-1,))
+        opt_config = opt_config[..., :neigh]
+
+    # append grid of size 1 to dimensions that are not yet included in the optimal grid.
+    opt_config = torch.cat((opt_config, torch.ones(batch_shape + (neigh - opt_config.shape[-1],)).to(opt_config.dtype)), dim=-1)
+    return opt_config[sort_idxs]
+
 
 def default_prune_tol(gmm: dd_dists.MixtureMultivariateNormal, factor: float = 0.5):
     stds = gmm.component_distribution.variance.mean(dim=-1).sqrt()  # [K]
