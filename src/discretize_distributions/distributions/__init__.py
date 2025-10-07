fafc974b203348ec8f63427b8cdf32663be500a4
sjladams <steven.adams.95@hotmail.com>
2025-10-07 13:51:19 +0200

    feat: add Categorical to __all__ exports in distributions module

--- src/discretize_distributions/distributions/__init__.py
+++ src/discretize_distributions/distributions/__init__.py
@@ -2,7 +2,10 @@ from .categorical_float import CategoricalFloat, CategoricalGrid, compress_categ
 from .multivariate_normal import MultivariateNormal, covariance_matrices_have_common_eigenbasis
 from .mixture import MixtureMultivariateNormal, compress_mixture_multivariate_normal, unique_mixture_multivariate_normal
 
+from torch.distributions import Categorical
+
 __all__ = [
+    'Categorical',
     'CategoricalFloat',
     'CategoricalGrid',
     'MultivariateNormal',
